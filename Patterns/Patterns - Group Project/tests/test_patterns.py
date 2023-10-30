307cfa6f7ec84cffcf67f46fd12c93ed170da315
21Saliji <147812319+21Saliji@users.noreply.github.com>
2023-10-30 10:20:46 +0200

    Add files via upload
    semi- final change
    doc strings are missing

---
+++ Patterns/Patterns - Group Project/tests/test_patterns.py
@@ -0,0 +1,139 @@
+import unittest
+from io import StringIO
+import sys
+from unittest.mock import patch
+from patterns import *
+
+class MyTestCase(unittest.TestCase):
+
+    def test_1_shape(self):
+    #Mocking to test if all lower cases are returned
+        sys.stdin = StringIO("TriAngLe\n")
+        self.assertEqual(get_shape(), "triangle")
+
+        sys.stdin = StringIO("SquAre\n")
+        self.assertEqual(get_shape(), "square")
+
+        sys.stdin = StringIO("PyRamiD\n")
+        self.assertEqual(get_shape(), "pyramid")
+
+    
+
+    # Test if get_shape only returns numbers below 80 only
+    @patch('builtins.input', side_effect = ["85","70", "100"])
+    def test_get_height(self,mock_input):
+        self.assertEqual(get_height(),70)
+
+
+    @patch('builtins.input', side_effect=['abc', 'xyz', '@', '/'])
+    def test_get_height(self, mock_input):
+        with self.assertRaises(ValueError):
+            get_height()
+
+    def test_4_square(self):
+        pass
+
+    def test_5_square(self):
+        
+        pass
+
+    def test_6_triangle(self):
+        """
+        test for reversed triangle with height 0f 3
+        """
+        with patch("sys.stdout", StringIO("1 1 1 \n2 2 \n3 \n")) as mock_stdout:
+            draw_triangle_reversed(3)
+            self.assertEqual(mock_stdout.getvalue(), "1 1 1 \n2 2 \n3 \n")
+    
+
+    def test_7_triangle(self):
+        """
+        test for reversed triangle
+        """
+        with patch("sys.stdout", StringIO("1 1 1 1 1 \n2 2 2 2 \n3 3 3 \n4 4 \n5 ")) as mock_stdout:
+            draw_triangle_reversed(5)
+            self.assertEqual(mock_stdout.getvalue(), "1 1 1 1 1 \n2 2 2 2 \n3 3 3 \n4 4 \n5 \n")
+
+    def test_8_triangle(self):
+        
+        pass
+    def test_9_triangle(self):
+        
+        pass
+
+    def test_10_triangle_multiplication(self):
+        """
+        test for an increamenting multiplication triangle 
+        """
+        with patch("sys.stdout", StringIO("1 \n2 4 \n3 6 9 \n4 8 12 16 \n")) as mock_stdout:
+            draw_triangle_multiplication(4)
+            self.assertEqual(mock_stdout.getvalue(), "1 \n2 4 \n3 6 9 \n4 8 12 16 \n")
+        
+
+    def test_11_triangle_multiplication(self):
+        """
+        test for an increamenting multiplication triangle 
+        """
+
+        with patch("sys.stdout", StringIO("1 \n2 4 \n3 6 9 \n4 8 12 16 \n5 10 15 20 25 \n6 12 18 24 30 36 \n")) as mock_stdout:
+            draw_triangle_multiplication(6)
+            self.assertEqual(mock_stdout.getvalue(), "1 \n2 4 \n3 6 9 \n4 8 12 16 \n5 10 15 20 25 \n6 12 18 24 30 36 \n")
+
+
+    @patch("sys.stdout", new_callable=StringIO)
+    # def test_12_pyramid(self):
+    def test_draw_pyramid(self, mock_stdout):
+        expected_output = "    *\n   ***\n  *****\n"
+        with patch("builtins.input", side_effect=["3"]):
+            draw_pyramid(3)
+            output = mock_stdout.getvalue()
+            self.assertEqual(output, expected_output)
+        
+
+    # def test_13_pyramid(self):
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_draw_pyramid(self, mock_stdout):
+        expected_output = "    *\n   ***\n  *****\n *******\n*********\n"
+        with patch("builtins.input", side_effect=["5"]):
+            draw_pyramid(5)
+            output = mock_stdout.getvalue()
+            self.assertEqual(output, expected_output)
+        pass
+
+    def test_14_primes(self):
+        # Test Case for prime numbers
+        self.assertTrue(draw_triangle_prime(2))
+        self.assertTrue(draw_triangle_prime(47))
+        self.assertTrue(draw_triangle_prime(193))
+
+        # Test Case for non-prime numbers
+        self.assertFalse(draw_triangle_prime(1))
+        self.assertFalse(draw_triangle_prime(60))
+        self.assertFalse(draw_triangle_prime(200))
+        
+        
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_15_prime(self, mock_stdout):
+        draw_triangle_prime(3)
+        output = mock_stdout.getvalue()
+        expected_output = "2 \n3 5 \n7 11 13 \n"
+        self.assertEqual(output, expected_output)
+        
+    def test_tower_of_hanoi_1(self):
+        pass
+
+    def test_tower_of_hanoi_2(self):
+        pass
+
+    def test_tower_of_hanoi_3(self):
+        pass
+    def test_tower_of_hanoi_4(self):
+        pass
+
+    def test_tower_of_hanoi_5(self):
+        pass
+
+        
+    
+if __name__ == '__main__':
+    unittest.main()
